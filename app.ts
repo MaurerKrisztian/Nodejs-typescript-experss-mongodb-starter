3d3bf0f6914545ee23c0d71d56ff35b8c8054fdd
Maurer Krisztian <krisztian.maurer@metrix.co.hu>
2021-01-18 10:29:16 +0100

    es6

--- app.ts
+++ app.ts
@@ -1,12 +1,19 @@
-const PORT = 3000;
+import mongoose from  'mongoose'
+import express from "express";
+import bodyParser from "body-parser";
 
-const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
+
 require('dotenv/config')
+const app = express();
+app.use(bodyParser.json());
 
-
+const PORT = 3000;
 const URI = process.env.DB_URI;
+
+if (URI === undefined) {
+    throw new Error('DB URI is undefined.')
+}
+
 mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true}, ()=>{
     console.log("connected to DB")
 });
